a530ed64c5373cd30ea9b4987effa1f578e42cd8
Mathias Remshardt <mathias.remshardt@logicline.de>
2020-12-22 11:47:08 +0100

    * adds material ui * updates ConferenceComponent to display list using material ui components

--- src/app/module.ts
+++ src/app/module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './component';
 import { ConferencesModule } from './conferences/module';
 import { AppRoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ConferencesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ConferencesModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
