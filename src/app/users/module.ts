2a533e751bf65028993dac7f8c78fb96706dc48b
Mathias Remshardt <mathias.remshardt@logicline.de>
2020-12-26 11:11:03 +0100

    * moves UsersComponent into dedicated component folder

--- src/app/users/module.ts
+++ src/app/users/module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { UsersComponent } from './container/component';
+import { UsersComponent } from './container/component/component';
 
 @NgModule({
   declarations: [UsersComponent],
   imports: [CommonModule, MatListModule],
-  exports: [UsersComponent],
+  exports: [UsersComponent]
 })
 export class UsersModule {}
