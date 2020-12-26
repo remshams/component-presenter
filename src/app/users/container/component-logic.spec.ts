29902f91e6e143d39f3f78912f8807ccea931477
Mathias Remshardt <mathias.remshardt@logicline.de>
2020-12-26 09:58:28 +0100

    * implements displaying usernames in UsersComponent * implements tests

--- src/app/users/container/component-logic.spec.ts
+++ src/app/users/container/component-logic.spec.ts
@@ -1,15 +1,46 @@
+import { of } from 'rxjs';
+import { createRxTestScheduler } from 'src/app/_common/test/test-helper';
+import { UsersBloc } from 'src/app/_common/users/bloc';
+import { createUsersFixture } from 'src/app/_common/users/fixture';
+import { createUsersRestAdapterMock } from 'src/app/_common/users/mock';
 import { UsersComponent } from './component';
 
 describe('UsersComponent - Logic', () => {
   let component: UsersComponent;
+  const users = createUsersFixture();
+  const userNames = users.map(user => user.name);
+  const usersRestAdapterMock = createUsersRestAdapterMock();
+  const usersBlock = new UsersBloc(usersRestAdapterMock);
 
   beforeEach(() => {
-    component = new UsersComponent();
+    usersRestAdapterMock.list.mockReset();
+    usersRestAdapterMock.list.mockReturnValue(of(users));
   });
 
-  describe('isUsers', () => {
-    it('should return true', () => {
-      expect(component.isUsers()).toEqual(true);
+  beforeEach(() => {
+    component = new UsersComponent(usersBlock);
+    component.ngOnInit();
+  });
+
+  describe('usernames$', () => {
+    it('should emit list of user names', () => {
+      createRxTestScheduler().run(({ expectObservable }) => {
+        expectObservable(component.usernames$).toBe('a', { a: userNames });
+      });
+    });
+    it('should emit empty list in case there are no users', () => {
+      createRxTestScheduler().run(({ expectObservable }) => {
+        usersRestAdapterMock.list.mockReturnValue(of([]));
+        component.ngOnInit();
+
+        expectObservable(component.usernames$).toBe('a', { a: [] });
+      });
+    });
+  });
+
+  describe('Init', () => {
+    it('should trigger refresh of user list in UsersBloc', () => {
+      expect(usersRestAdapterMock.list).toHaveBeenCalled();
     });
   });
 });
