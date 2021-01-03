2092eed1e6c1ee82f3e25a0f5983a2defe10d155
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-01-03 11:29:10 +0100

    * adds deducated folders for list and menu * moves files

--- src/app/users/module.ts
+++ src/app/users/module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
-import { UsersComponent } from './container/component/component';
-import { UsersMenuComponent } from './container/menu/component';
-import { UsersWithPresenterComponent } from './container/presenter/component';
+import { UsersComponent } from './list/component/component';
+import { UsersWithPresenterComponent } from './list/presenter/component';
+import { UsersMenuComponent } from './menu/component';
 import { UsersRoutingModule } from './routing.module';
 
 @NgModule({
