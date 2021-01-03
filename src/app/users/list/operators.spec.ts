d6ac73aecb8190ebf28395fbd15b518e96265ed4
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-01-03 12:00:29 +0100

    * adds numberOfUsers$ to UsersComponentPresenter

--- src/app/users/list/operators.spec.ts
+++ src/app/users/list/operators.spec.ts
@@ -1,7 +1,7 @@
 import { createRxTestScheduler } from '@app/common/test/test-helper';
 import { createUsersFixture } from '@app/common/users/fixture';
 import { of } from 'rxjs';
-import { extractUserNames } from './operators';
+import { extractNumberOfUsers, extractUserNames } from './operators';
 
 describe('UsersOperators', () => {
   const users = createUsersFixture();
@@ -16,6 +16,21 @@ describe('UsersOperators', () => {
     it('should emit empty list in case user list is empty', () => {
       createRxTestScheduler().run(({ expectObservable }) => {
         expectObservable(of([]).pipe(extractUserNames())).toBe('(a|)', { a: [] });
+      });
+    });
+  });
+
+  describe('extractNumberOfUsers', () => {
+    const numberOfUsers = users.length;
+
+    it('should emit number of users', () => {
+      createRxTestScheduler().run(({ expectObservable }) => {
+        expectObservable(of(users).pipe(extractNumberOfUsers())).toBe('(a|)', { a: users.length });
+      });
+    });
+    it('should emit 0 in case there are no users', () => {
+      createRxTestScheduler().run(({ expectObservable }) => {
+        expectObservable(of([]).pipe(extractNumberOfUsers())).toBe('(a|)', { a: 0 });
       });
     });
   });
--- src/app/users/list/presenter/presenter.ts
+++ src/app/users/list/presenter/presenter.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { UsersService } from '@app/common/users/service';
 import { Observable } from 'rxjs';
-import { extractUserNames } from '../operators';
+import { extractNumberOfUsers, extractUserNames } from '../operators';
 
 @Injectable()
 export class UsersComponentPresenter {
   readonly usernames$: Observable<ReadonlyArray<string>>;
+  readonly numberofUsers$: Observable<number>;
 
   constructor(private usersService: UsersService) {
     this.usernames$ = this.setupUserNames();
+    this.numberofUsers$ = this.setupUserCount();
 
     this.onInit();
   }
 
   private setupUserNames(): Observable<ReadonlyArray<string>> {
     return this.usersService.users$.pipe(extractUserNames());
+  }
+
+  private setupUserCount(): Observable<number> {
+    return this.usersService.users$.pipe(extractNumberOfUsers());
   }
 
   private onInit(): void {
