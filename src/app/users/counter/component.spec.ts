ed1560229a363812fec4e918fd2d46bc4a405d79
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-01-03 11:32:46 +0100

    * adds UserCounterStub

---
+++ src/app/users/counter/component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UsersCounterComponent } from './component';
+
+describe('UsersCounterComponent', () => {
+  let component: UsersCounterComponent;
+  let fixture: ComponentFixture<UsersCounterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UsersCounterComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersCounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
